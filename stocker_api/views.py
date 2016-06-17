bee31cde5a91f53c2d26383cf926f7726f87ce2f
k24dizzle <zhaok24@uw.edu>
2016-06-17 15:22:26 -0700

    pulls historical data

--- stocker_api/views.py
+++ stocker_api/views.py
@@ -24,19 +24,33 @@ def get_stock(request):
 def get_stock(request):
     stock = request.path.split('/')[-1]
     url = 'https://www.google.com/finance?q=' + stock
-    hist_url = 'https://www.google.com/finance/historical?q=NASDAQ%3A' + stock
     text = requests.get(url).text
     soup = bs4.BeautifulSoup(text, "html.parser")
     data = {}
     data['current_price'] = soup.select('.pr')[0].getText().split('\n')[1]
     data['open_price'] = soup.select('.snap-data .val')[2].getText().split('\n')[0]
     data['day_change'] = soup.select('.chr')[0].getText()
+
+    data['historical_data'] = get_historical_data(stock)
+
     json_data = json.dumps(data)
     today = datetime.datetime.now().date()
     fifty = timedelta(days=-50)
 
-    print fifty + today
     return HttpResponse(json_data)
+
+def get_historical_data(stock):
+    hist_url = 'https://www.google.com/finance/historical?q=NASDAQ%3A' + stock + '&num=200'
+    hist_text = requests.get(hist_url).text
+    hist_soup = bs4.BeautifulSoup(hist_text, "html.parser")
+    dates = hist_soup.select('.historical_price tr')
+    dates_process = dates[0].getText().split('\n\n')
+    result = {}
+    for i in range(1, len(dates_process)):
+        results = dates_process[i].split('\n')
+        result[results[0]] = {'Open': results[1], 'High': results[2], 'Low': results[3], 'Close': results[4], 'Volume': results[5]}
+    return result
+
 
 def round_hund(num):
     return ("%.2f" % num)
