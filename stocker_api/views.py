ea3053ec10dec3f945fba885b7a15aee26ed94e6
k24dizzle <zhaok24@uw.edu>
2016-06-17 15:05:56 -0700

    switched to google finance

--- stocker_api/views.py
+++ stocker_api/views.py
@@ -4,6 +4,8 @@ import urllib
 import json
 import bs4
 import requests
+import datetime
+from datetime import timedelta
 # Create your views here.
 
 """
@@ -21,8 +23,23 @@ def get_stock(request):
 
 def get_stock(request):
     stock = request.path.split('/')[-1]
-    url = 'http://www.marketwatch.com/investing/stock/' + stock
+    url = 'https://www.google.com/finance?q=' + stock
+    hist_url = 'https://www.google.com/finance/historical?q=NASDAQ%3A' + stock
     text = requests.get(url).text
     soup = bs4.BeautifulSoup(text, "html.parser")
-    price = soup.select('.pricewrap .data')[0].getText()
-    return HttpResponse(price)
+    data = {}
+    data['current_price'] = soup.select('.pr')[0].getText().split('\n')[1]
+    data['open_price'] = soup.select('.snap-data .val')[2].getText().split('\n')[0]
+    data['day_change'] = soup.select('.chr')[0].getText()
+    json_data = json.dumps(data)
+    today = datetime.datetime.now().date()
+    fifty = timedelta(days=-50)
+
+    print fifty + today
+    return HttpResponse(json_data)
+
+def round_hund(num):
+    return ("%.2f" % num)
+
+def percent_change(old, new):
+    return round_hund(((new - old) / old) * 100)
