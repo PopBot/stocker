267ac530d92dd00e6eedd7f6e597818f2cd01c37
k24dizzle <zhaok24@uw.edu>
2016-06-20 11:16:19 -0700

    sort by date now

--- stock_app/static/js/stocker.js
+++ stock_app/static/js/stocker.js
@@ -1,17 +1,31 @@
 $(document).ready(function() {
     console.log('it works');
-    $.getJSON('/api/get_stock/AAPL', function(data) {
-        $('#name').html(data.name);
-        $('#current').html(data.current_price);
-        var hist_data = data.historical_data;
-        var keys = Object.keys(hist_data);
-        for (var i = 0; i < keys.length; i++) {
-            var report = '<p>' + keys[i] + ': ' + hist_data[keys[i]]['Open'] + ': ' + hist_data[keys[i]]['Close'] + '</p>';
-            var temp  = $(report);
-            $("#data").append(temp);
+    $("#submit").click(function(e) {
 
-        }
+        e.preventDefault();
+        var stock = $('#get_stock').val();
+    
+        $.getJSON('/api/get_stock/' + stock, function(data) {
+            $('#name').html(data.name);
+            $('#data').empty();
+            $('#current').html(data.current_price);
+            var hist_data = data.historical_data;
+            var keys = Object.keys(hist_data);
+            keys.sort(function(a, b){
+                return new Date(b) - new Date(a);
+
+            });
+            console.log(keys);
+            for (var i = 0; i < keys.length; i++) {
+                var report = '<p>' + keys[i] + ': ' + hist_data[keys[i]]['Open'] + ': ' + hist_data[keys[i]]['Close'] + '</p>';
+                var temp  = $(report);
+                $("#data").append(temp);
+
+            }
+        });
     });
 
 
 });
+
+
